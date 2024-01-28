e307b9f6c0ed520d7ef44dd2f5838363ec13900e
Hwngne <Xhung2k3@gmail.com>
2024-01-28 20:44:05 +0700

    I changed 2 file

--- dblogin.sql
+++ dblogin.sql
@@ -1,1 +1,2 @@
-INSERT INTO dblogin.tblogin VALUES('Xhung2411',' Xuanhung2k3@'), ('Hwngne', 'Xuanhung2k3@'), ('Kobiet', 'Xuanhung2k3@');
+INSERT INTO dblogin.tbloginadmin VALUES(1, 'Xhung2411','Xuanhung2k3@');
+insert into dblogin.tbloginuser values(1, 'Taolakhach', '123456');
--- handle.php
+++ handle.php
@@ -4,13 +4,13 @@ $password = $_POST['password'];
 
 $conn = new mysqli('localhost', 'root', 'Xuanhung2k3@', 'dblogin');
 
-$sql = "select * from dblogin.tblogin where username = '$username'";
+$sql = "select * from dblogin.tbloginadmin where username = '$username'";
 $result = $conn->query($sql)->fetch_assoc();
 ?>
 <?php if ($result['password'] == $password): ?>
     <script>
         alert("Đăng nhập thành công");
-        window.location.replace("https://www.facebook.com/hung.2411.2003");
+        window.location.replace("admin/homepage.php");
     </script>
 <?php else: ?>
     <script>alert("Đăng nhập thất bại")</script>
